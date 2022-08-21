9b7368eba784a33156e4d052e3b8926e2652ae1c
davidbarbi3r <davidbarbi3r@gmail.com>
2022-08-21 19:55:18 +0200

    :sparkles: Add validation to Post routes

--- src/routes/Post.ts
+++ src/routes/Post.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import controller from '../controllers/Post';
+import { Schemas, ValidateSchema } from '../middleware/ValidateSchema';
 
 const router = express.Router();
 
-router.post('/create', controller.createPost);
+router.post('/create', ValidateSchema(Schemas.post.create), controller.createPost);
 router.get('/get/:postId', controller.readPost);
 router.get('/get', controller.readAll);
-router.patch('/update/:postId', controller.updatePost);
+router.patch('/update/:postId', ValidateSchema(Schemas.post.update), controller.updatePost);
 router.delete('/delete/:postId', controller.deletePost);
 
 export = router;
